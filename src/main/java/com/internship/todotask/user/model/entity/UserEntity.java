4c3bcb2840008e0dcaff7ae6af45cd9f7eb7eae8
Serhiy Haleliuk <serggalel@gmail.com>
2026-06-11 13:49:59 +0300

    TDT-9-Fix-missed-parts

--- src/main/java/com/internship/todotask/user/model/entity/UserEntity.java
+++ src/main/java/com/internship/todotask/user/model/entity/UserEntity.java
@@ -17,14 +17,19 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "role", nullable = false)
     @Convert(converter = ConverterEnum.class)
     private Role role;
 
--- src/main/java/com/internship/todotask/user/repository/UserRepository.java
+++ src/main/java/com/internship/todotask/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.internship.todotask.user.repository;
 
 import com.internship.todotask.user.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,7 +15,15 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findUserEntityById(Long id);
 
+    Optional<UserEntity> findUserEntityByEmail(String email);
+
+    void deleteUserEntityById(Long id);
+
     @Query(value = "SELECT user_id FROM user_task WHERE task_id = :taskId", nativeQuery = true)
     Set<Long> getUserIdsByTaskId(@Param("taskId") Long taskId);
+
+    @Modifying
+    @Query(value = "DELETE FROM user_task WHERE user_id = :userId AND task_id = :taskId", nativeQuery = true)
+    void deleteCollaborator(@Param("userId") Long userId, @Param("taskId") Long taskId);
 
 }
