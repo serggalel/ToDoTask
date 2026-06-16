1d81c3b0fb37363fc8945884740e6370e7ba8e51
Serhiy Haleliuk <serggalel@gmail.com>
2026-06-16 13:53:13 +0300

    TDT-11-Create-UserController

---
+++ src/main/java/com/internship/todotask/user/controller/UserController.java
@@ -0,0 +1,99 @@
+package com.internship.todotask.user.controller;
+
+import com.internship.todotask.user.model.dto.UserCollabDto;
+import com.internship.todotask.user.model.dto.UserDto;
+import com.internship.todotask.user.model.entity.UserEntity;
+import com.internship.todotask.user.service.UserOperationsService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserOperationsService userOperationsService;
+
+    @PostMapping("/register")
+    @ResponseBody
+    public String registerUser(@Valid @RequestBody UserDto userDto) {
+        return userOperationsService.register(userDto);
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public ResponseEntity<Page<UserEntity>> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        Page<UserEntity> pageableEntities = userOperationsService.getPaginatedUsers(page, size, sortBy, direction);
+        return ResponseEntity.ok(pageableEntities);
+    }
+
+    @GetMapping("/get/{id}")
+    @ResponseBody
+    public Optional<UserEntity> getUser(@PathVariable Long id) {
+        return userOperationsService.getUserById(id);
+    }
+
+    @PostMapping("/update/{id}")
+    @ResponseBody
+    public String updateUser(
+            @PathVariable Long id,
+            @Valid @RequestBody UserSecurityDto userDto
+            ) {
+        return userOperationsService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public String deleteUser(@PathVariable Long id) {
+        return userOperationsService.deleteUser(id);
+    }
+
+    @GetMapping("/getCollabs/{taskId}")
+    @ResponseBody
+    public ResponseEntity<Page<UserCollabDto>> getAllCollaborators(
+            @PathVariable Long taskId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        Page<UserCollabDto> userCollabDtoPage = userOperationsService.getAllCollaborators(taskId, page, size, sortBy, direction);
+        return ResponseEntity.ok(userCollabDtoPage);
+    }
+
+    @GetMapping("/getPossCollabs/{taskId}")
+    @ResponseBody
+    public List<UserCollabDto> getPossibleCollaborators(@PathVariable Long taskId) {
+        return userOperationsService.getPotentialCollaborators(taskId);
+    }
+
+    @DeleteMapping("/rmvCollab")
+    @ResponseBody
+    public String removeCollaborator(
+            @RequestParam Long taskId,
+            @RequestParam Long userId
+    ) {
+        return userOperationsService.removeCollaborator(userId, taskId);
+    }
+
+    @PostMapping("/addCollab")
+    @ResponseBody
+    public String addCollaborator(
+            @RequestParam Long userId,
+            @RequestParam Long taskId
+    ) {
+        return userOperationsService.addCollaborator(userId, taskId);
+    }
+
+}
